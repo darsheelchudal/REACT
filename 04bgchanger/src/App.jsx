95265b81d4ee377160139fc16652efe063013c69
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-06 21:21:17 +0545

    04bgchanger

--- 04bgchanger/src/App.jsx
+++ 04bgchanger/src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+const [color,setColor] = useState("olive");
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-red">
-      Hello world!
-    </h1>
-    </>
+      <div className="w-full h-screen duration-200" style={{backgroundColor: color}}>
+       
+<div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2"></div>
+      </div>
   )
 }
 
