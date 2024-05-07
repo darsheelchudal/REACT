1727f021cef7f53f1feac145ef4a913dc9f0cebe
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-07 18:20:16 +0545

    passwordGenerator

--- 06bgchanger/src/App.jsx
+++ 06bgchanger/src/App.jsx
@@ -1,12 +1,22 @@
 import React, {useState} from "react";
-import './App.css';
+
 
 function App() {
 const [color,setColor] =  useState("olive");
   return (
     <div className="w-full h-screen duration-200" style={{backgroundColor:color}}>
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        
+        <div className="flex flex-wrap justify-center gap-3 shadow-xl bg-white rounded-xl px-3 py-2">
+         <button onClick={() => {
+          setColor("red");
+         }} className="px-4 py-1 bg-red-800 rounded-full text-white">Red</button>
+         <button onClick={() => (setColor("green"))} className="px-4 py-1 bg-green-800 rounded-full text-white">Green</button>
+         <button onClick={() => (setColor("blue"))}  className="px-4 py-1 bg-blue-800 rounded-full text-white">Blue</button>
+         <button onClick={() => (setColor("yellow"))}  className="px-4 py-1 bg-yellow-800 rounded-full text-white">Yellow</button>
+
+
+
+        </div>
          </div>
       </div>
   )
--- 07passwordGenerator/src/App.jsx
+++ 07passwordGenerator/src/App.jsx
@@ -1,13 +1,85 @@
-
-
+import React, { useState, useCallback, useEffect } from "react";
 
 function App() {
+  const [length, setLength] = useState(8);
+  const [numberAllowed, setNumberAllowed] = useState(false);
+  const [characterAllowed, setCharacterAllowed] = useState(false);
+  const [password, setPassword] = useState("");
 
+  
+
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    if (numberAllowed) {
+      str = str + "0123456789";
+    }
+    if (characterAllowed) {
+      str += "!@#$%^&*()";
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let char = Math.floor(Math.random() * str.length);
+      pass += str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, characterAllowed]);
+
+  useEffect(() => {passwordGenerator();}, [length,numberAllowed,characterAllowed,passwordGenerator]);
   return (
-    <h1 className="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-  )
+    <div className="w-full h-screen bg-white">
+      <div className="w-full max-w-md mx-auto rounded-lg p-10 px-6 my-8 text-orange-500 bg-gray-500">
+        <h1 className="text-white text-center my-4">Password Generator</h1>
+        <div className="flex rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+         
+        </div>
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={6}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Length : {length}</label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={numberAllowed}
+              id="numberInput"
+              onChange={() => {
+                setNumberAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="numberInput"> Numbers </label>
+          </div>
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={characterAllowed}
+              id="characterInput"
+              onChange={() => {
+                setCharacterAllowed((prev) => !prev);
+              }}
+            />
+            <label htmlFor="characterInput"> Characters </label>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
