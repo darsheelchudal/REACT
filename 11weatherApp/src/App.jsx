f62456db5cf585b0301630073aa09f6e5837a26f
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-10 13:52:35 +0545

    weatherApp

---
+++ 11weatherApp/src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from "react";
+
+import "./App.css";
+
+const api = {
+  key: "e7296d109e0ead1aac198cb95749ea2a",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div className="content">
+        <h1>Hello, world!</h1>
+        <p>This is some content.</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
