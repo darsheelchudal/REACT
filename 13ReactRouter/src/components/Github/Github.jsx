c5f1f92cfa46f5e16401f1a8e2afebd86f927f81
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-11 07:40:45 +0545

    final Commit

--- 13ReactRouter/src/components/Github/Github.jsx
+++ 13ReactRouter/src/components/Github/Github.jsx
@@ -1,26 +1,34 @@
 import React from "react";
 import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
 
 function Github() {
-  const [data, setData] = useState({});
-  useEffect(() => {
-    fetch(`https://api.github.com/users/darsheelchudal`)
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data);
-        setData(data);
-      });
-  }, []);
+  const data = useLoaderData();
+  //   const [data, setData] = useState({});
+  //   useEffect(() => {
+  //     fetch(`https://api.github.com/users/darsheelchudal`)
+  //       .then((response) => response.json())
+  //       .then((data) => {
+  //         console.log(data);
+  //         setData(data);
+  //       });
+  //   }, []);
   return (
     <>
       <div className="text-center m-4 bg-gray-400 text-white text-3xl font-bold">
         Github Name: {data.name}
       </div>
-      <div className="text-center m-4 bg-gray-400 text-white text-3xl font-bold">
+      <div className="text-center m-4 bg-gray-400 text-white text-3xl font-bold p-4">
         Github followers: {data.followers}
+        <img src={data.avatar_url} alt="git profile image" width={300} />
       </div>
     </>
   );
 }
 
 export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/darsheelchudal");
+  return response.json();
+};
