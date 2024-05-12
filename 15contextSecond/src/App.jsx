a654bea830506f27a45e8cbac93e3b2ec13b9f85
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-12 07:33:34 +0545

    dark mode enabled

---
+++ 15contextSecond/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+import "./App.css";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+import { useEffect } from "react";
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  //actual change in a theme
+  useEffect(() => {
+    document.querySelector("html").classList.remove("light", "remove");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+          <ThemeBtn />
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
