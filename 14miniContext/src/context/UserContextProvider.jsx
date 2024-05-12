d98145484696079dfc840d6d363efa9a26c5d703
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-12 05:54:37 +0545

    contextAPI(createContext)

---
+++ 14miniContext/src/context/UserContextProvider.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from "react";
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState([]);
+  return;
+  <UserContext.Provider>{children}</UserContext.Provider>;
+};
+
+export default UserContextProvider;
