5377cec3dc212d46c4ca0532a8fad21f191dec48
Darsheel Chudal <74804981+darsheelchudal@users.noreply.github.com>
2024-05-12 14:16:31 +0545

    NotesApp

---
+++ 16NoteApp/src/App.jsx
@@ -0,0 +1,104 @@
+import { useState } from "react";
+
+import "./App.css";
+function App() {
+  const [title, setTitle] = useState("");
+  const [desc, setDesc] = useState("");
+  const [notes, setNotes] = useState([]);
+  const [count, setCount] = useState(0);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const addNote = () => {
+    if (!title || !desc) {
+      setErrorMessage("Enter required input");
+      return;
+    }
+    setNotes([...notes, { key: count, title: title, desc: desc }]);
+    setCount(count + 1);
+    setTitle("");
+    setDesc("");
+    setErrorMessage("");
+  };
+
+  return (
+    <>
+      <div className="w-screen h-max flex items-center mx-[40%] my-20">
+        <div className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md">
+          <div className="relative mx-4 -mt-6 mb-4 grid h-28 place-items-center overflow-hidden rounded-xl bg-gradient-to-tr from-pink-600 to-pink-400 bg-clip-border text-white shadow-lg shadow-pink-500/40">
+            <h5 className="block font-sans text-xl font-semibold leading-snug tracking-normal text-white antialiased">
+              Notes Applications
+            </h5>
+          </div>
+          <div className="flex flex-col gap-4 p-6">
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeh
+                older=" "
+                onChange={(e) => {
+                  setDesc(e.target.value);
+                }}
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Add Titles
+              </label>
+            </div>
+            <div className="relative h-11 w-full min-w-[200px]">
+              <input
+                className="peer h-full w-full rounded-md border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-3 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-pink-500 focus:border-t-transparent focus:outline-0 disabled:border-0 disabled:bg-blue-gray-50"
+                placeholder=" "
+                onChange={(e) => {
+                  setTitle(e.target.value);
+                }}
+              />
+              <label className="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[4.1] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-pink-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:!border-pink-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:!border-pink-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
+                Description
+              </label>
+            </div>
+          </div>
+          <div className="p-6 pt-0">
+            <button
+              className="block w-40 select-none rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none"
+              type="button"
+              data-ripple-light="true"
+              onClick={addNote}
+            >
+              Add Notes
+            </button>
+            <span className="error text-red-900 flex items-center w-100">
+              **** {errorMessage}
+            </span>
+          </div>
+          {notes.map((note, index) => (
+            <div
+              key={index}
+              className="mx-20 my-3 w-60 h-40 border-pink-400 border-4 flex items-center p-4"
+            >
+              <div className="notes">
+                <h1>Title: {notes.title}</h1>
+                <p>Note: {notes.desc}</p>
+                <button
+                  className="block w-20 select-none rounded-lg bg-gradient-to-tr from-pink-600 to-pink-400 py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-white shadow-md shadow-pink-500/20 transition-all hover:shadow-lg hover:shadow-pink-500/40 active:opacity-[0.85] disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none my-4"
+                  type="button"
+                  data-ripple-light="true"
+                  onClick={addNote}
+                >
+                  Delete
+                </button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/@material-tailwind/html@latest/styles/material-tailwind.css"
+      />
+
+      <script src="https://unpkg.com/@material-tailwind/html@latest/scripts/ripple.js"></script>
+    </>
+  );
+}
+
+export default App;
